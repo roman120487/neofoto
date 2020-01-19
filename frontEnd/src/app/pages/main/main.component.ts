fd937e89a4f26461349d8af00e75d46333eac974
roman120487 <roman120487@ukr.net>
2020-01-19 13:22:38 +0200

    make scroll button

--- frontEnd/src/app/pages/main/main.component.ts
+++ frontEnd/src/app/pages/main/main.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
 
+export class MainComponent implements OnInit {
+  scrollBtn: boolean = false;
   constructor() { }
 
   ngOnInit() {
+
   }
+
+  @HostListener('window:scroll', ['$event']) onscroll(event): void {
+    const top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+    if (top > 0) {
+      this.scrollBtn = true;
+    } else {
+      this.scrollBtn = false;
+    }
+  }
+
+  scrollUp(): void {
+    const scroll = setInterval(scrollStep, 20);
+
+    function scrollStep() {
+      const top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+      if (top > 0) {
+        window.scrollBy(0, -100);
+      } else {
+        clearInterval(scroll);
+      }
+    }
+  }
+
+
+
+
 
 }
