d8586f3c7848642305a09f8da9389fafeb535564
Andrii Nyvchyk <37506960+andriinyvchyk@users.noreply.github.com>
2020-02-01 17:10:23 +0200

    hosting

--- backEnd/routing/response.js
+++ backEnd/routing/response.js
@@ -9,7 +9,7 @@ const Response = require('../models/response')
 router.use(cors())
 
 var corsOptions = {
-    origin: 'http://localhost'
+    origin: 'http://188.40.170.11:3001'
   }
 
 const storageConfig = multer.diskStorage({
@@ -48,7 +48,7 @@ router.post('/', cors(), function (req, res) {
         'response': date.response,
         'author': date.author,
         'idImg': req.file.filename,
-        'idImgUrl': 'http://localhost:3000/uploads/response/'
+        'idImgUrl': 'http://188.40.170.11:3001/uploads/response/'
     });
     emp.save((err, doc) => {
         if (!err) { res.send(doc); }
