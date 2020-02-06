66f842fac7d9cd3243620aa85cafd0e01351bdd0
roman120487 <roman120487@ukr.net>
2020-02-06 10:17:11 +0200

    lang 2

--- frontEnd/src/app/pages/poslugi/poslugi.component.ts
+++ frontEnd/src/app/pages/poslugi/poslugi.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GallaryService } from 'src/app/shared/services/gallary.service';
 
 @Component({
   selector: 'app-poslugi',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PoslugiComponent implements OnInit {
 
-  constructor() { }
+  constructor( private lagService: GallaryService) { }
 
   ngOnInit() {
   }
